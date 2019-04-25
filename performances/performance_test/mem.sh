f18f5e4be7f11302d96361177b9b61a5c821ddd6
y² <yy@yuyuan.org>
2019-04-25 22:46:54 +0000

    [cpu] 10->5 subs, 10->3 experiments

--- performances/performance_test/mem.sh
+++ performances/performance_test/mem.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 ROS2_SDK_INSTALL_PATH="/home/ubuntu/___/ros2_release-latest_20190424_ws/install"
-
 ROS2_PERFORMANCE_TEST_INSTALL_PATH="/home/ubuntu/___/irobot_perf_with_no_690/ws/install"
 
 . $ROS2_SDK_INSTALL_PATH/local_setup.bash
 . $ROS2_PERFORMANCE_TEST_INSTALL_PATH/local_setup.bash
 . env_no690.sh
-export MAX_PUBLISHERS=1
-export MAX_SUBSCRIBERS=5
-export MSG_TYPES=10b
-export PUBLISH_FREQUENCY=100
-export DURATION=30
-export NUM_EXPERIMENTS=3
-export MON_CPU_RAM=1
-bash scripts/pub_sub_ros2.sh
 
-echo -e "\033[36mpython scripts/plot_scripts/cpu_ram_plot.py <path_to_experiments> --x subs --y cpu --skip 5\033[0m"
+export MAX_PUBLISHERS=1
+export MAX_SUBSCRIBERS=1
+export MSG_TYPES="10b 100b 250b 1kb 10kb 250kb 1mb 4mb 8mb"
+export DURATION=10
+export NUM_EXPERIMENTS=3
+bash scripts/only_subs.sh
+
+echo -en "\033[36m"
+echo -en "python3 scripts/plot_scripts/cpu_ram_plot.py results/only_subs_ros2/cpu_ram* --x subs --y rss --y2 vsz"
+echo -e "\033[0m"
 
